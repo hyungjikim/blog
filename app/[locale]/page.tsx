cd402f7f4412d287e2a67625dc1ec3dc881acbd1
hyungji.k <icthelight@naver.com>
2024-12-15 22:21:51 +0900

    ✨ #8 feat: stylex applied

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,8 +1,17 @@
+import stylex from '@stylexjs/stylex';
+
 export default async function Page({
   params,
 }: {
   params: Promise<{ locale: string }>;
 }) {
   const { locale } = await params;
-  return <>locale : {locale}</>;
+  return <h1 {...stylex.props(style.h1)}>locale : {locale}</h1>;
 }
+
+const style = stylex.create({
+  h1: {
+    fontSize: '2rem',
+    fontWeight: 700,
+  },
+});
